b20179b9217081c238dbc2d5349da04956f8118a
Pierre-Antoine Fechet <22179913+pafechet@users.noreply.github.com>
2021-09-06 16:27:49 +0200

    Update main.py
    include date

--- kepler/main.py
+++ kepler/main.py
@@ -1,10 +1,13 @@
 from keplergl import KeplerGl
 import pandas as pd
 import geopandas as gpd
+from datetime import datetime
 
-df = pd.read_excel("ABCimages_namelist.xlsx", sheet_name='Sheet1')
+df = pd.read_excel('ABCimages_namelist.xlsx', sheet_name='Sheet1')
 map1 = KeplerGl(height=800)
 
+df['CAMPAIGN_DATE'] = df.apply(lambda row: datetime.strptime(row['CAMPAIGN'][-6:],'%Y %b'), axis = 1)
+df.to_csv('data.csv')
 # Create a geopandas dataframe from a regular dataframe
 gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.LON, df.LAT))
 # Add data to Kepler
